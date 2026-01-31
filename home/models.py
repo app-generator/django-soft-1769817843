cb9215c6d421955a0daf1939a975e25a2964036b
App Generator <51070104+app-generator@users.noreply.github.com>
2026-01-31 02:04:12 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Personeller(models.Model):
+
+    #__Personeller_FIELDS__
+    adsoyad = models.TextField(max_length=255, null=True, blank=True)
+    telno = models.TextField(max_length=255, null=True, blank=True)
+    adresi = models.TextField(max_length=255, null=True, blank=True)
+    tcno = models.TextField(max_length=255, null=True, blank=True)
+
+    #__Personeller_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Personeller")
+        verbose_name_plural = _("Personeller")
+
+
+
+#__MODELS__END
